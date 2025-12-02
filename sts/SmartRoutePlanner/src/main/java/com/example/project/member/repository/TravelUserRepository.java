619a545ef5fc83bedae002ca99826cc0df0ae40f
lealle <lealle@naver.com>
2025-12-02 22:16:43 +0900

    fix_setting

--- sts/SmartRoutePlanner/src/main/java/com/example/project/member/repository/TravelUserRepository.java
+++ sts/SmartRoutePlanner/src/main/java/com/example/project/member/repository/TravelUserRepository.java
@@ -3,6 +3,7 @@ package com.example.project.member.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.project.member.domain.TravelUser;
 
